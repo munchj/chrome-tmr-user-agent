30e56c659f5d689e205cf4cd939f64f81b8d1555
Julien MUNCH <jmunch@wideagency.com>
2024-01-17 20:50:03 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,27 @@
+const rules = {
+  removeRuleIds: [1],
+  addRules: [
+    {
+      id: 1,
+      priority: 1,
+      action: {
+        type: "modifyHeaders",
+        requestHeaders: [
+          {
+            header: "user-agent",
+            operation: "set",
+            value: `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36`,
+          },
+        ],
+      },
+      condition: {
+        resourceTypes: ["main_frame"],
+        urlFilter: "*",
+      },
+    },
+  ],
+};
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.declarativeNetRequest.updateDynamicRules(rules);
+});
